d92365c7cb7a00607ebad18fff15557a1a02a781
iDhruvJha <dhruvjha397@gmail.com>
2025-12-22 23:06:54 +0530

    update variable

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int kittu(int oji[], int size , int key){
+int arr(int nums[], int size , int key){
     int start = 0 ;
     int end = size-1;
     int mid = (start+end)/2;
     while(start<=end){
-        if(key==oji[mid]){
+        if(key==nums[mid]){
             return mid;
         }
-        if(key>oji[mid]){
+        if(key>nums[mid]){
             start = mid+1;
         }
         else{
@@ -21,12 +21,10 @@ int kittu(int oji[], int size , int key){
 }
 int main(){
 
-    int oji[8] = {4,6,7,17,45,66,85,89};
+    int nums[8] = {4,6,7,17,45,66,85,89};
     int dj[9] = {5,8,19,55,78,87,91,98,100};
 
-    int rohan = kittu(oji,8,6);
-    cout << "the entered key is at : " << rohan <<"\n";
+    int num = arr(nums,8,6);
+    cout << "the entered key is at : " << num <<"\n";
 
-    int heee = kittu(dj,9,109);
-    cout << "the entered key is at : " << heee ;
 }
